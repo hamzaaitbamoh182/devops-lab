bd60cf9be7342b8a7f7318e53326592da1ff8487
hamzaaitbamoh182 <h.aitbamoh@Alexsys.solutions>
2025-07-29 12:02:39 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// Load the built-in http module
+const http = require('http');
+
+const PORT = 3000;
+
+// Create a simple HTTP server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World from Node.js!\n');
+});
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
